50be08e066288ad2a1225fbb3c81868f7eb1b64f
Itz-Rafin <23201227@uap-bd.edu>
2026-05-10 02:36:48 +0600

    Updated setting.py

--- crisishive/settings.py
+++ crisishive/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-$1v1qy*@1$2z6miz_=-hmc1*hucizfs09zss_etw$&d2p8fisn
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
